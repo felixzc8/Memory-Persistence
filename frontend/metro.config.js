d1ce8658e1292ef26886db6ab5944d015bda5fbd
Felix <felixzc8@gmail.com>
2025-07-01 11:15:14 -0700

    remove react native frontend

--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
-
-// Add resolver configuration for better Node.js compatibility
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
- 
-module.exports = withNativeWind(config, { input: './app/globals.css' })
