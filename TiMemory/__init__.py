035cf49e040ac8d569397b26131e47f005cb8231
Felix <felixzc8@gmail.com>
2025-08-01 11:04:35 -0700

    merge summary table to sesison table

--- TiMemory/__init__.py
+++ TiMemory/__init__.py
@@ -2,12 +2,11 @@ from .timemory import TiMemory
 from .config.base import MemoryConfig
 from .schemas import Memory, MemoryResponse, MemoryAttributes
 from .schemas.session import Session, SessionMessage, SessionSummary, CreateSessionRequest, CreateSessionResponse, UpdateSessionRequest, SessionListResponse
-from .schemas.summary import SummaryResponse, CreateSummaryRequest
-from .models import Memory as MemoryModel, Base, Session as SessionModel, Message as MessageModel, Summary as SummaryModel
+from .models import Memory as MemoryModel, Base, Session as SessionModel, Message as MessageModel
 
 __all__ = [
     "TiMemory", "MemoryConfig", 
     "Memory", "MemoryResponse", "MemoryAttributes",
-    "Session", "SessionMessage", "SessionSummary", "CreateSessionRequest", "CreateSessionResponse", "UpdateSessionRequest", "SessionListResponse", "SummaryResponse", "CreateSummaryRequest",
-    "MemoryModel", "Base", "SessionModel", "MessageModel", "SummaryModel"
+    "Session", "SessionMessage", "SessionSummary", "CreateSessionRequest", "CreateSessionResponse", "UpdateSessionRequest", "SessionListResponse",
+    "MemoryModel", "Base", "SessionModel", "MessageModel"
 ]
